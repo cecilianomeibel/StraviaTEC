585da2d333aeb779cbf3be79c544d0176602bb11
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2023-11-03 10:45:50 -0600

    Reusable friend cards

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NavBarComponent } from './reusables/nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserHomeComponent } from './user-view/user-home/user-home.component';
+import { FriendCardComponent } from './reusables/friend-card/friend-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     UserHomeComponent,
+    FriendCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/reusables/friend-card/friend-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'friend-card',
+  templateUrl: './friend-card.component.html',
+  styleUrls: ['./friend-card.component.css'],
+})
+export class FriendCardComponent {
+}
