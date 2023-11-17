9216d9e63ffd2f27a9ef6749e53ca234d11142fc
Meibel Ceciliano <cecilianomeibel@gmail.com>
2023-11-17 16:16:51 -0700

    Endpoints Category

---
+++ WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebAPI.Models;
+using WebAPI.Utils;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly SqlServerConnector _sqlServerConnector;
+
+        public CategoryController(SqlServerConnector sqlServerConnector)
+        {
+            _sqlServerConnector = sqlServerConnector;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            // Asegúrate de que el nombre del stored procedure sea correcto
+            string storedProcedureName = "SP_CATEGORY_CRUD";
+
+            // Crea los parámetros necesarios para el stored procedure
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+            new SqlParameter("@statementType", "ReadAll"),
+            new SqlParameter("@name", DBNull.Value)  // Agrega un parámetro @name con un valor predeterminado
+            };
+
+            // Ejecuta el stored procedure y devuelve la lista de Category
+            List<Category> category = await _sqlServerConnector.ExecuteStoredProcedureListAsync<Category>(storedProcedureName, parameters);
+            return Ok(category);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetCategoryByName(string name)
+        {
+            // Asegúrate de que el nombre del stored procedure sea correcto
+            string storedProcedureName = "SP_CATEGORY_CRUD";
+
+            // Crea los parámetros necesarios para el stored procedure
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+            new SqlParameter("@statementType", "ReadOnlyOne"),
+            new SqlParameter("@name", name)
+            };
+
+            // Ejecuta el stored procedure y devuelve la Category con el name proporcionado
+            Category category = await _sqlServerConnector.ExecuteStoredProcedureSingleAsync<Category>(storedProcedureName, parameters);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] Category category)
+        {
+            // Asegúrate de que el nombre del stored procedure sea correcto
+            string storedProcedureName = "SP_CATEGORY_CRUD";
+
+            // Crea los parámetros necesarios para el stored procedure
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+            new SqlParameter("@statementType", "Create"),
+            new SqlParameter("@name", category.name)
+           
+            };
+
+            // Ejecuta el stored procedure y devuelve la respuesta
+            await _sqlServerConnector.ExecuteStoredProcedureSingleAsync<object>(storedProcedureName, parameters);
+            return Ok();
+        }
+
+
+
+        [HttpPut("{name}")]
+        public async Task<IActionResult> UpdateCategory(string name, [FromBody] Category category)
+        {
+            // Asegúrate de que el nombre del stored procedure sea correcto
+            string storedProcedureName = "SP_CATEGORY_CRUD";
+
+            // Crea los parámetros necesarios para el stored procedure
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+            new SqlParameter("@statementType", "Update"),
+            new SqlParameter("@name", name),         
+            new SqlParameter("@newName", category.name)
+
+
+            };
+
+            // Ejecuta el stored procedure y devuelve la respuesta
+            await _sqlServerConnector.ExecuteStoredProcedureSingleAsync<object>(storedProcedureName, parameters);
+            return Ok();
+        }
+
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteCategory(string name)
+        {
+            // Asegúrate de que el nombre del stored procedure sea correcto
+            string storedProcedureName = "SP_CATEGORY_CRUD";
+
+            // Crea los parámetros necesarios para el stored procedure
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+            new SqlParameter("@statementType", "Delete"),
+            new SqlParameter("@name", name)
+            };
+
+            // Ejecuta el stored procedure y devuelve la respuesta
+            await _sqlServerConnector.ExecuteStoredProcedureSingleAsync<object>(storedProcedureName, parameters);
+            return Ok();
+        }
+    }
+}
