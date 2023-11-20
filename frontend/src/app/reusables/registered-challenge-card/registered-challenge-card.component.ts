c110325e4b52753fd94374bb4f58b1088e0f5021
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2023-11-19 18:44:57 -0600

    Registered challenges connection to endpoint

--- frontend/src/app/reusables/registered-challenge-card/registered-challenge-card.component.ts
+++ frontend/src/app/reusables/registered-challenge-card/registered-challenge-card.component.ts
@@ -8,10 +8,35 @@ import { Component, Input } from '@angular/core';
 export class RegisteredChallengeCardComponent {
   @Input() challenge!: {    
     id: number,
-    activityName: string,
+    activityType: string,
     name: string,
     period: string,
     type: string,
     access: string
   }
+
+  daysleft:any;
+
+  ngOnInit(){
+    this.tiempoRestante(this.challenge.period);
+  }
+
+  tiempoRestante(final:string){
+    // Fecha de finalización
+    const fechaFinalizacionString = final;
+    const fechaFinalizacion = new Date(fechaFinalizacionString);
+
+    // Fecha actual
+    const fechaActual = new Date();
+
+    // Calcula la diferencia en milisegundos
+    const diferenciaEnMilisegundos = fechaFinalizacion.getTime() - fechaActual.getTime();
+
+    // Convierte la diferencia a días
+    const diasFaltantes = Math.ceil(diferenciaEnMilisegundos / (1000 * 60 * 60 * 24));
+
+    this.daysleft = diasFaltantes;
+
+  }
 }
+
